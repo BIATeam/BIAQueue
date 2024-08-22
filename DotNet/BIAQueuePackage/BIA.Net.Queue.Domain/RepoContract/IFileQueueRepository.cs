d260e7d86d026db942533d6012817b0d4810b452
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-08-22 11:36:16 +0200

    Init BIANetQueue

---
+++ DotNet/BIAQueuePackage/BIA.Net.Queue.Domain/RepoContract/IFileQueueRepository.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="IFileQueueRepository.cs" company="BIA">
+//  Copyright (c) BIA.Net. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Queue.Domain.RepoContract
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading;
+    using BIA.Net.Queue.Domain.Dto.FileQueue;
+    using BIA.Net.Queue.Domain.Dto.Queue;
+
+    /// <summary>
+    /// Interface of FileQueueRepository.
+    /// </summary>
+    public interface IFileQueueRepository
+    {
+        /// <summary>
+        /// Configure the queues.
+        /// </summary>
+        /// <param name="topics">The list of <see cref="TopicDto"/>.</param>
+        void Configure(IEnumerable<TopicDto> topics);
+
+        /// <summary>
+        /// Subscribe to receive <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="observer">The <see cref="IObserver{T}"/> of <see cref="FileQueueDto"/>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>An iDisposable.</returns>
+        IDisposable Subscribe(IObserver<FileMessageDto> observer, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Subscribe to receive <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="observer">The <see cref="IObserver{T}"/> of <see cref="FileQueueDto"/>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <param name="user">The user identifier.</param>
+        /// <param name="password">The user password.</param>
+
+        /// <returns>An iDisposable.</returns>
+        IDisposable Subscribe(IObserver<FileMessageDto> observer, CancellationToken cancellationToken, string user, string password);
+
+        /// <summary>
+        /// Send a new <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="topic">The global information to read a RabbitMQ Topic.</param>
+        /// <param name="file"> The file.</param>
+        /// <returns>A <see cref="bool"/>.</returns>
+        bool SendFile(TopicDto topic, FileMessageDto file);
+
+        /// <summary>
+        /// Send a new <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="topic">The global information to read a RabbitMQ Topic.</param>
+        /// <param name="file"> The file.</param>
+        /// <param name="user">The user identifier.</param>
+        /// <param name="password">The user password.</param>
+        /// <returns>A <see cref="bool"/>.</returns>
+        bool SendFile(TopicDto topic, FileMessageDto file, string user, string password);
+    }
+}
---
+++ DotNet/BIAQueuePackage/BIA.Net.Queue.Infrastructure.Service/Repositories/FileQueueRepository.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="FileQueueRepository.cs" company="BIA">
+//  Copyright (c) BIA.Net. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Queue.Infrastructure.Service.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using BIA.Net.Queue.Common.Observer;
+    using BIA.Net.Queue.Domain.Dto.FileQueue;
+    using BIA.Net.Queue.Domain.Dto.Queue;
+    using BIA.Net.Queue.Domain.RepoContract;
+    using BIA.Net.Queue.Infrastructure.Service.Helpers;
+
+    /// <summary>
+    /// Repository for file queuing.
+    /// </summary>
+    public sealed class FileQueueRepository : IObservable<FileMessageDto>, IFileQueueRepository
+    {
+        private readonly List<IObserver<FileMessageDto>> observers;
+        private IEnumerable<TopicDto> topics;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileQueueRepository"/> class.
+        /// </summary>
+        /// <param name="endpoint">The endpoint address of queue Server.</param>
+        /// <param name="queueName">The queue name listen.</param>
+        public FileQueueRepository()
+        {
+            this.observers = new List<IObserver<FileMessageDto>>();
+        }
+
+        /// <inheritdoc />
+        public void Configure(IEnumerable<TopicDto> topics)
+        {
+            if (topics == null)
+            {
+                throw new ArgumentException("queues cannot be null");
+            }
+
+            this.topics = topics;
+        }
+
+        /// <inheritdoc />
+        public IDisposable Subscribe(IObserver<FileMessageDto> observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IDisposable Subscribe(IObserver<FileMessageDto> observer, CancellationToken cancellationToken)
+        {
+            // Check whether observer is already registered. If not, add it
+            if (!this.observers.Contains(observer))
+            {
+                this.observers.Add(observer);
+
+                foreach (TopicDto topic in this.topics)
+                {
+                    RabbitMQHelper.ReceiveMessageAsync<FileMessageDto>(topic, observer.OnNext, cancellationToken);
+                }
+            }
+
+            return new Unsubscriber<FileMessageDto>(this.observers, observer);
+        }
+
+        /// <inheritdoc />
+        public IDisposable Subscribe(IObserver<FileMessageDto> observer, CancellationToken cancellationToken, string user, string password)
+        {
+            // Check whether observer is already registered. If not, add it
+            if (!this.observers.Contains(observer))
+            {
+                this.observers.Add(observer);
+
+                foreach (TopicDto topic in this.topics)
+                {
+                    RabbitMQHelper.ReceiveMessageAsync<FileMessageDto>(topic, observer.OnNext, cancellationToken, user, password);
+                }
+            }
+
+            return new Unsubscriber<FileMessageDto>(this.observers, observer);
+        }
+
+        /// <inheritdoc />
+        public bool SendFile(TopicDto topic, FileMessageDto file)
+        {
+             return RabbitMQHelper.SendMessage(topic, file);
+        }
+
+        /// <inheritdoc />
+        public bool SendFile(TopicDto topic, FileMessageDto file, string user, string password)
+        {
+            return RabbitMQHelper.SendMessage(topic, file, user, password);
+        }
+    }
+}
